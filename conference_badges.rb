bc22417578c096da88339cfde0e191fa892a6ee4
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-15 18:04:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,11 +4,5 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(arr_attendees)
-  count = 0
-  badges = nil
-  arr_attendees.each do |i| 
-    badges= badge_maker(arr_attendees[i])
-    count += 1
-  end
-  badges
+
 end
